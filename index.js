406eeb05bc62818810d7e4a3e1bb6cc98d47474b
owen <owen.chosen@gmail.com>
2023-08-14 12:11:12 +0100

    Done

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i]+array[j] === target && i!=j) return true
+    }
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  Write the Big O time complexity of your function here: 
+      Big O time complexity of my hasTargetSum function is: O(n^2)
 */
+
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here: 
+    iterate from the beginning of the array
+      for each element in the array, iterate from the beginning of the array
+        if the sum of the element in outer loop and this inner loop is equal to the target
+          return true
+      if the iterations on both loops is completed and the consition isn't satidsfied    
+        return false
 */
 
+
 /*
-  Add written explanation of your solution here
+  Add written explanation of your solution here:
+    iterate from the beginning of the array using a for loop. Inside this for loop,
+    for each element in the array, iterate from the beginning of the array again using 
+    another for loop
+      if the sum of the element in outer loop and this inner loop is equal to the target
+        return true
+    if the iterations on both loops is completed and the consition isn't satidsfied    
+      return false
 */
 
 // You can run `node index.js` to view these console logs
